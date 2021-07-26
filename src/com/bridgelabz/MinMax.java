dec0051df200a758fd0c38c9a4a5782b77609a78
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-27 00:41:08 +0530

    Added: Method to check max string

--- src/com/bridgelabz/MinMax.java
+++ src/com/bridgelabz/MinMax.java
@@ -1,16 +1,28 @@
 package com.bridgelabz;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class MinMax {
 	
 	public static void main(String[] args) {
 	      Integer[] numbers = { 8, 2, 7};
 	      Integer maxint = (int) Collections.max(Arrays.asList(numbers));
-	      Float[] num = {2.3f, 2.23f, 1.1f};
+	      Float[] num = {2.3f, 4.7f, 1.1f};
 	      Float maxfloat = (float) Collections.max(Arrays.asList(num));
-
+	      String[] fruits = {"Apple" ,"Watermelon", "Kiwi"};
+	      int a = fruits[0].length();
+	      int b = fruits[1].length();
+	      int c = fruits[2].length();
+	      if (a > b && a > c) {
+	    	  System.out.println(fruits[0]);
+	      }
+	      else if (b > a && b > c) {
+	    	  System.out.println(fruits[1]);
+	      }
+	      else {
+	    	  System.out.println(fruits[2]);
+	      }
+	      
 	      toPrint(maxfloat);	     
 	      toPrint(maxint);
 	   }
